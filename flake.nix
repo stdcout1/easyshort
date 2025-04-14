26ad108262c27f53739409c76b26f65d9c34b663
nasir <nasirkhimani@gmail.com>
2025-04-14 17:50:41 -0400

    flake for building

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          packages.default = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "adthand";
+            version = "1";
+            src = ./.;
+            useFetchCargoVendor = true;
+            cargoHash = ""; 
+          };
+
+        }
+      );
+}
+
